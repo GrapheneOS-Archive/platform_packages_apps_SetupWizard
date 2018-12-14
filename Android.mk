1f9864a3c2ab3d1cefe82c4c5245877d3bdb79f8
Chirayu Desai <chirayudesai1@gmail.com>
2018-12-14 18:55:18 +0530

    Change package name
    * Since we are going to be forking this and modifying the code,
      let's also change the package name while we're at it to keep
      things clearer.
    * Also, we will still be able to git merge since git tracked
      the rename and should be able to figure things out. We'll see.

--- Android.mk
+++ Android.mk
@@ -5,7 +5,7 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 LOCAL_MODULE_TAGS := optional
 
-LOCAL_PACKAGE_NAME := LineageSetupWizard
+LOCAL_PACKAGE_NAME := CalyxSetupWizard
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_OVERRIDES_PACKAGES := Provision
---
+++ src/org/calyxos/setupwizard/SetupWizardActivity.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright (C) 2016 The CyanogenMod Project
+ * Copyright (C) 2017,2019 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.calyxos.setupwizard;
+
+import static android.content.Intent.FLAG_GRANT_READ_URI_PERMISSION;
+
+import static org.calyxos.setupwizard.SetupWizardApp.ACTION_LOAD;
+import static org.calyxos.setupwizard.SetupWizardApp.EXTRA_SCRIPT_URI;
+import static org.calyxos.setupwizard.SetupWizardApp.LOGV;
+
+import android.annotation.Nullable;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.setupcompat.util.WizardManagerHelper;
+
+import org.calyxos.setupwizard.util.SetupWizardUtils;
+import org.calyxos.setupwizard.wizardmanager.WizardManager;
+
+public class SetupWizardActivity extends BaseSetupWizardActivity {
+    private static final String TAG = SetupWizardActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (LOGV) {
+            Log.v(TAG, "onCreate savedInstanceState=" + savedInstanceState);
+        }
+        if (SetupWizardUtils.hasGMS(this)) {
+            SetupWizardUtils.disableHome(this);
+            finish();
+        } else if (WizardManagerHelper.isUserSetupComplete(this)) {
+            SetupWizardUtils.finishSetupWizard(this);
+            finish();
+        } else {
+            onSetupStart();
+            SetupWizardUtils.enableComponent(this, WizardManager.class);
+            Intent intent = new Intent(ACTION_LOAD);
+            if (isPrimaryUser()) {
+                intent.putExtra(EXTRA_SCRIPT_URI, getString(R.string.calyx_wizard_script_uri));
+            } else {
+                intent.putExtra(EXTRA_SCRIPT_URI, getString(R.string.calyx_wizard_script_user_uri));
+            }
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | FLAG_GRANT_READ_URI_PERMISSION);
+            startActivity(intent);
+            finish();
+        }
+    }
+}
