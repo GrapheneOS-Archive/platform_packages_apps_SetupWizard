23c5bae5fc367082e407175cca785c78b390c01f
Daniel Micay <danielmicay@gmail.com>
2020-12-11 16:34:19 -0500

    remove Play services support

--- src/org/lineageos/setupwizard/util/SetupWizardUtils.java
+++ src/org/lineageos/setupwizard/util/SetupWizardUtils.java
@@ -63,18 +63,12 @@ import org.lineageos.setupwizard.SimMissingActivity;
 import org.lineageos.setupwizard.WifiSetupActivity;
 import org.lineageos.setupwizard.wizardmanager.WizardManager;
 
-import org.lineageos.internal.util.PackageManagerUtils;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class SetupWizardUtils {
 
     private static final String TAG = SetupWizardUtils.class.getSimpleName();
-
-    private static final String GMS_PACKAGE = "com.google.android.gms";
-    private static final String GMS_SUW_PACKAGE = "com.google.android.setupwizard";
-    private static final String GMS_TV_SUW_PACKAGE = "com.google.android.tungsten.setupwraith";
 
     private static final String PROP_BUILD_DATE = "ro.build.date.utc";
 
@@ -168,22 +162,6 @@ public class SetupWizardUtils {
         } else {
             Log.i(SetupWizardApp.TAG, "Skip enabling notfications - StatusBarManager is null");
         }
-    }
-
-    public static boolean hasGMS(Context context) {
-        String gmsSuwPackage = hasLeanback(context) ? GMS_TV_SUW_PACKAGE : GMS_SUW_PACKAGE;
-
-        if (PackageManagerUtils.isAppInstalled(context, GMS_PACKAGE) &&
-                PackageManagerUtils.isAppInstalled(context, gmsSuwPackage)) {
-            PackageManager packageManager = context.getPackageManager();
-            if (LOGV) {
-                Log.v(TAG, GMS_SUW_PACKAGE + " state = " +
-                        packageManager.getApplicationEnabledSetting(gmsSuwPackage));
-            }
-            return packageManager.getApplicationEnabledSetting(gmsSuwPackage) !=
-                    COMPONENT_ENABLED_STATE_DISABLED;
-        }
-        return false;
     }
 
     public static boolean isPackageInstalled(Context context, String packageName) {
